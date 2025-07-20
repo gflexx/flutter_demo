db18006723b9289e5f1ca2970f139aa6e586c60d
gflexx <glennenri24@gmail.com>
2025-07-20 15:03:13 +0300

    Updated profile page with list view list tile

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
+const int itemCount = 18;
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text('Profile Page'),
-        ],
-      ),
+    return ListView.builder(
+      itemCount: itemCount,
+      padding: const EdgeInsets.all(2.7),
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: Icon(Icons.person),
+          title: Text('Person ${index + 1}'),
+          subtitle: Text('Brief info on person ${index + 1} ...'),
+          trailing: Icon(Icons.arrow_forward_ios),
+          onTap: (){
+            debugPrint('Tapped on person ${index + 1}');
+          },
+        );
+      },
     );
   }
 }
