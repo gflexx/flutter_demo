01531caaa8ffe769a8c9ee3dcb78c891fc209952
gflexx <glennenri24@gmail.com>
2025-07-20 13:01:17 +0300

    Added network image and checkbox

--- lib/learn_flutter.dart
+++ lib/learn_flutter.dart
@@ -8,6 +8,9 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitched = false;
+  bool? isCheckbox = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,29 +32,50 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
             width: double.infinity,
             padding: const EdgeInsets.all(9),
             child: const Text(
-              'Some long long text as im trying to get a feel of this',
+              'Some long long text as im trying to get a feel of this, its understandable and in a week I cn wip an app',
             ),
           ),
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: isSwitched ? Colors.blue : Colors.lime,
+            ),
             onPressed: () {
               debugPrint('Btn Pressed');
             },
             child: const Text('Click Me'),
           ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Outline Clicked');
-            },
-            child: const Text('Outline Click'),
-          ),
+          // OutlinedButton(
+          //   onPressed: () {
+          //     debugPrint('Outline Clicked');
+          //   },
+          //   child: const Text('Outline Click'),
+          // ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Icon(Icons.local_fire_department),
               const Text('Some services provided'),
-              Icon(Icons.local_gas_station)
+              Icon(Icons.local_gas_station),
             ],
-          )
+          ),
+          Switch(
+            value: isSwitched,
+            onChanged: (bool newBoolean) {
+              setState(() {
+                isSwitched = newBoolean;
+              });
+            },
+          ),
+          const Text('Checkbox'),
+          Checkbox(
+            value: isCheckbox,
+            onChanged: (bool? newBoolean) {
+              setState(() {
+                isCheckbox = newBoolean;
+              });
+            },
+          ),
+          Image.network('https://git-scm.com/images/logo@2x.png')
         ],
       ),
     );
